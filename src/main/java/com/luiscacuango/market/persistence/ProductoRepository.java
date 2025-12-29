cc1b68f2b98c94bebf04f0cc6fb4e0da4ff69bb2
Luis Cacuango <manuel.cacuango@est.umet.edu.ec>
2025-12-29 15:30:43 -0500

    feat: Creación de Consultas avanzadas con Query Methods en Spring Data

--- src/main/java/com/luiscacuango/market/persistence/ProductoRepository.java
+++ src/main/java/com/luiscacuango/market/persistence/ProductoRepository.java
@@ -4,11 +4,25 @@ import com.luiscacuango.market.persistence.crud.ProductoCrudRepository;
 import com.luiscacuango.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getALL() {
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRepository.findByIdCantidadStockLessThanAndEstado(cantidad, true);
+
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
     }
 }
