2c230f02d1defb6caf3eb9647521e4a54ef12df2
Luis Cacuango <manuel.cacuango@est.umet.edu.ec>
2025-12-29 18:18:33 -0500

    feat: Creación de Mapeadores en Java para Conversión de Compras y Productos

--- src/main/java/com/luiscacuango/market/domain/Purchase.java
+++ src/main/java/com/luiscacuango/market/domain/Purchase.java
@@ -10,7 +10,10 @@ public class Purchase {
     private String paymentMethod;
     private String comment;
     private String state;
-    private List<PurchaseItem> item;
+    private List<PurchaseItem> items;
+
+    // Getters y setters
+
 
     public int getPurchaseId() {
         return purchaseId;
@@ -60,11 +63,11 @@ public class Purchase {
         this.state = state;
     }
 
-    public List<PurchaseItem> getItem() {
-        return item;
+    public List<PurchaseItem> getItems() {
+        return items;
     }
 
-    public void setItem(List<PurchaseItem> item) {
-        this.item = item;
+    public void setItems(List<PurchaseItem> items) {
+        this.items = items;
     }
 }
