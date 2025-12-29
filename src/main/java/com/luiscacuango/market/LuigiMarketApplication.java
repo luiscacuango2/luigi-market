dbe0f3b24e5a6caba618611e883b56482877ecce
Luis Cacuango <manuel.cacuango@est.umet.edu.ec>
2025-12-29 14:09:35 -0500

    feat: Creación de la estructura del proyecto

---
+++ src/main/java/com/luiscacuango/market/LuigiMarketApplication.java
@@ -0,0 +1,13 @@
+package com.luiscacuango.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuigiMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuigiMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luiscacuango/market/LuigiMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiscacuango.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuigiMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
