4ea9b6734da94533497bc6eb5392e2cd08cbf440
Luis Cacuango <manuel.cacuango@est.umet.edu.ec>
2025-12-29 17:36:17 -0500

    feat: Modelado de Dominio para Gestión de Compras en Supermercados

---
+++ src/main/java/com/luiscacuango/market/domain/repository/PurchaseRepository.java
@@ -0,0 +1,12 @@
+package com.luiscacuango.market.domain.repository;
+
+import com.luiscacuango.market.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
