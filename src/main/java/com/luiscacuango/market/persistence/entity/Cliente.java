d892c5bd6195e5389082d4c9086dc84e128abc31
Luis Cacuango <manuel.cacuango@est.umet.edu.ec>
2025-12-29 15:11:50 -0500

    feat: Creación de Relaciones en JPA

--- src/main/java/com/luiscacuango/market/persistence/entity/Cliente.java
+++ src/main/java/com/luiscacuango/market/persistence/entity/Cliente.java
@@ -2,6 +2,8 @@ package com.luiscacuango.market.persistence.entity;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "clientes")
 public class Cliente {
@@ -20,6 +22,9 @@ public class Cliente {
 
     @Column(name = "correo_electronico")
     private String correoElectronico;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Compra> compras;
 
     public String getIdCliente() {
         return idCliente;
